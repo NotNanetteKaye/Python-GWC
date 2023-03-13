85726bcae3b7ec7688ee9417fda0ce824a474f14
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-12 23:34:45 -0500

    Testing classes in main.py

--- code_demo/main.py
+++ code_demo/main.py
@@ -4,4 +4,10 @@ from collar import Collar
 
 Toffee = Perro()
 pink_collar = Collar()
+pink_collar.color = 'pink'
 coffee = Toy()
+
+
+Toffee.collar.append(pink_collar)
+
+print(Toffee.collar[0].color)
