dc960933d8dc3eed71c441c27328daaf617dd40b
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-16 16:41:06 -0500

    Limpié main.py para mostrar solo métodos

--- GWC/main.py
+++ GWC/main.py
@@ -18,21 +18,4 @@ inventory1.book_name.append(book1)
 inventory1.book_status.append(book1_status.book_status)
 # print(inventory1.book_status[1])
 
-for i in range(len(inventory1.book_name)):
-    print(f'Book Name: {inventory1.book_name[i].book_name}')
-    try:
-        print(f'Book Status: {inventory1.book_status[i].book_status}')
-    except:
-        print(f'Book Status: {inventory1.book_status[i]}')
-
-# this can also work as well: inventory1.book_status[1] = 'checked out'
-inventory1.book_status[1] = book1_status.update_book_status()
-
-for i in range(len(inventory1.book_name)):
-    print(f'Book Name: {inventory1.book_name[i].book_name}')
-    try:
-        print(f'Book Status: {inventory1.book_status[i].book_status}')
-    except:
-        print(f'Book Status: {inventory1.book_status[i]}')
-
-# book1.run_inventory()
+inventory1.loop_through_lists()
