63161fa37ec2af54e68e7c635f649b7b6465cb07
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-16 16:49:17 -0500

    limpié library_inventory.py

--- GWC/library_inventory.py
+++ GWC/library_inventory.py
@@ -8,50 +8,9 @@ class Library_Inventory:
     
     def loop_through_lists(self):
         for i in range(len(self.book_name)):
-            print(f'Book Name: {self.book_name[i].book_name}')
+            print(f'\nBook Name: {self.book_name[i].book_name}')
             try:
-                print(f'Book Status: {self.book_status[i].book_status}')
+                print(f'Book Status: {self.book_status[i].book_status}\n\n')
             except:
-                print(f'Book Status: {self.book_status[i]}')
-        pass
-
-#    def display_welcome(self):
-        print('Welcome to the library inventory! Here you will find all the books and their status.')
-        pass
-
-#    def book_mode(self): 
-        print('Would you like to add a book or see current available books?')
-        user_input = input('Please enter 1 for adding new books or 2 to see current books: ')
-        if user_input == '1' or user_input == '2':
-            if user_input == '1':
-                book_title = Books(input('Please enter book title: '))
-                self.book_name.append(book_title)
-                print('Book successfully added!')
-            elif user_input == '2':
-                if len(self.book_name) == 0:
-                    print('There are currently no books in the inventory. Would you like to add one? ')
-                    user_input = input('Please enter 1 for yes or 2 for no: ')
-                    if user_input == '1' or user_input == '2':
-                        if user_input == '1':
-                            self.book_name = Books(input('Please enter book title: '))
-                            print('Book successfully added!')
-                        elif user_input == '2':
-                            print('Okay, have a nice day!')
-                    else:
-                        print('Invalid option. Try again.')
-                        self.book_mode()
-        else:
-            print('Invalid option. Try again')
-            self.book_mode()
-        pass
-
-#    def current_inventory(self):
-        print('Current books in the inventory: ')
-        for book in range(len(self.book_name)):
-            print[book]
-
-#    def run_inventory(self):
-        self.display_welcome()
-        self.book_mode()
-        self.current_inventory()
+                print(f'Book Status: {self.book_status[i]}\n\n')
         pass
