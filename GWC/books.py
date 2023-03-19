518dfd0a534dcf01ecb65638936bced81209485b
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-19 13:51:59 -0500

    Updated method in Books.py

--- GWC/books.py
+++ GWC/books.py
@@ -2,18 +2,23 @@
 class Book:
     def __init__(self, book_name):
         self.book_name = book_name
-        self.book_status = 2
+        self.book_status = 2         # 2 sets default status of book to 'On Shelf'
         pass
 
     def update_book_status(self):
-        user_input = int(input(f'\nUpdating {self.book_name}\'s status. Please enter 1 for \'Checked Out\' or 2 for \'On Shelf\' status. Default is \'On Shelf\': '))
-        if user_input == 1 or user_input == 2:
+        user_input = input(f'\nUpdating {self.book_name}\'s status. Please enter 1 for \'Checked Out\' or 2 for \'On Shelf\' status. Default is \'On Shelf\': ')
+        if user_input == '1' or user_input == '2':
+            user_input = int(user_input)
             if user_input == 1:
                 self.book_status -= 1
                 print(f'Updated {self.book_name}\'s to Checked Out!' )  
                 return(self.book_status) 
             elif user_input == 2:
-                print(f'Kept {self.book_name}\'s book status to On Shelf!')
+                if self.book_status == 1:
+                    self.book_status += 1
+                    print(f'Updated {self.book_name}\'s book status to On Shelf!')
+                else:
+                    print(f'Kept {self.book_name}\'s status to On Shelf!')
         else:
             print('Invalid option. Please try again.')
             self.update_book_status()
--- GWC/main.py
+++ GWC/main.py
@@ -1,6 +1,10 @@
 from library_inventory import Library_Inventory
 from books import Book
 
+# Requirements
+# Structure of Program
+# Demo Program
+
 inventory0 = Library_Inventory('Nanette Kaye')
 
 book0 = Book('Dr. Seuss')
@@ -17,3 +21,6 @@ inventory0.loop_through_inventory()
 inventory1 = Library_Inventory('Autodesk') # Create multiple inventories 
 inventory1.append_book_to_inventory(book0) # Append same book to multiple inventories
 inventory1.loop_through_inventory() # Inventory 1 would carry the same book status from Inventory 0 of 'Checked Out'
+book0.update_book_status()
+inventory1.loop_through_inventory()
+inventory0.loop_through_inventory()
