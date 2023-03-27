58228d2f08188fbfd0a1c1f761c23860bd2d4285
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-03-26 21:39:38 -0500

    🤔 WATCH YT EXPLANATION

---
+++ GWC_w_Dictionary/library_dictionary.py
@@ -0,0 +1,11 @@
+# The user can print out a list of each book and its status (“on shelf” or “checked out”)
+# The user can add new books to the inventory.
+# ✅: The default status of newly added books will be “on shelf”
+# The user can toggle the status of each book between “on shelf” and “checked out”
+
+book = {
+    'book_name': '',
+    'book_status': 'On Shelf',
+}
+# python class v dictionary?
+# youtube explanation: https://www.youtube.com/watch?v=8SCYteAlqko
